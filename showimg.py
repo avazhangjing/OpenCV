34f583cc15fba515fb515c454534e60554040e44
avazhangjing <avazhangjing@126.com>
2018-01-08 09:17:08 +0800

    showing.py: add plt show img
    showing.py: add plt show img

--- showimg.py
+++ showimg.py
@@ -25,4 +25,20 @@ if __name__ == '__main__':
     cv2.destroyAllWindows()
     #cv2.destroyAllWindows() ： 这个函数就是关闭窗口，
     #和（4）连起来就是按下任意键关闭显示图片的窗口。
+
+
+    
+    #接下来，我们用matplotlib显示图片
+    img2 = cv2.imread('D:\pic\yosemeti.jpg')
+    plt.imshow(img2,cmap='gray',interpolation='bicubic')
+    #plt.imshow(): 这个函数用于将图片放入显示窗口中，但并不会弹出窗口。
+    #有三个参数：图像的nparray，显示类型cmap以及插值算法。
+    #前两个参数前面介绍过了。第三个参数是用于选择插值算法，
+    #填补图像由于拉伸放大造成的不平滑的像素块，保持一定的图片质量。
+    #此处选择的是双三次插值算法，是一种精度很高但速度偏慢的算法。
+    plt.show()
+    #cv2.imread()读入彩色图像时，返回的nparray是按照B,G,R的顺序排列的，
+    #而matplotlib中的plt.imshow()显示彩色图像时，是按照RGB的顺序显示的，
+    #因此就会造成红蓝色的错乱
+    
     
